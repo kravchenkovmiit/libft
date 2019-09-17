203d491449b4c1bbb15620963b4aa5b498c96744
hvalenci <53438861+kravchenkovmiit@users.noreply.github.com>
2019-09-17 14:48:32 +0300

    file created

---
+++ ft_arrnew.c
@@ -0,0 +1,22 @@
+#include "libft.h"
+#include <stdlib.h>
+
+char    **ft_arrnew(size_t len)
+{
+    char    **fresh;
+    size_t  i;
+    
+    i = 0;
+    if (len + 1 == 0)
+        return (NULL);
+    fresh = (char *)malloc(sizeof(char *) * (len + 1));
+    if (fresh == NULL)
+        return (NULL);
+    while (len--)
+    {
+        fresh[i] = 0;
+        i++;
+    }
+    fresh[i] = 0;
+    return (fresh);
+}
