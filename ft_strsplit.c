910940da27e8931fb50cf583e385ec022f10b167
hvalenci <53438861+kravchenkovmiit@users.noreply.github.com>
2019-09-17 15:17:58 +0300

    file updated

--- ft_strsplit.c
+++ ft_strsplit.c
@@ -25,18 +25,51 @@ static int ft_counter(char const *s, char c)
 	return(1 + ft_counter(s, c));
 }
 
+static	char	*ft_()
+{
+	size_t	i;
+
+	i = 0;
+	while (s[i] && s[i] != c)
+		i++;
+	fresh[j] = ft_strnew(i);
+	if (fresh == NULL)
+	{
+		ft_arrdel(fresh, j);
+		return (NULL);
+	}
+	fresh[j] = ft_strncpy(s, i);
+	s += i;
+	return (s);
+}
+
 char	**ft_strsplit(char const *s, char c)
 {
 	size_t	strnum;
 	char	**fresh;
+	size_t	j;
 
 	if (c == '\0' || s == NULL || *s == '\0')
 		return (NULL);
 	strnum = 0;
+	j = 0;
 	if (*s != c)
 		strnum = 1;
 	strnum += ft_counter(s, c);
 	fresh = ft_arrnew(strnum);
 	if (fresh == NULL)
 		return (NULL);
+	while (strnum--)
+	{
+		if (*s != c)
+		{
+			s = ft_addstringtoarr(fresh[j], s);
+			if (s == NULL)
+				return (NULL);
+			j++;
+		}
+		while (*s == c)
+			s++;
+	}
+	return (fresh);
 }
