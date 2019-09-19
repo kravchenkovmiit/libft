6eb87530d41fa8ed48aab7c5b563752366a74af0
hvalenci <53438861+kravchenkovmiit@users.noreply.github.com>
2019-09-19 15:49:14 +0300

    Create ft_lstnew.c

---
+++ ft_lstnew.c
@@ -0,0 +1,29 @@
+#include "libft.h"
+#include <stdlib.h>
+
+t_list	*ft_lstnew(void const *content, size_t content_size)
+{
+	t_list	*lst;
+	
+	list = (t_list *)malloc(sizeof(*lst));
+	if (lst == NULL)
+		return (NULL);
+	if (content == NULL)
+	{
+		lst->content = NULL;
+		lst->content_size = 0;
+	}
+	else
+	{
+		lst->content = malloc(content_size);
+		if (!lst->content)
+		{
+			free(lst);
+			return (NULL);
+		}
+		ft_memcpy(lst->content, content, content_size);
+		lst->content_size = content_size;
+	}
+	lst->next = NULL;
+	return (lst);
+}
